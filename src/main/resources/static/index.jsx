a708df14f0b25b6b3ea0579179113eb6b9d6fe36
bud manstrong <nbpeth@gmail.com>
2017-03-04 12:41:34 -0600

    react view started

---
+++ src/main/resources/static/index.jsx
@@ -0,0 +1,11 @@
+var title = (
+    <div className="container-fluid bg-primary">
+        <h1>
+            Budget Master 9000
+        </h1>
+    </div>
+);
+
+ReactDOM.render(
+  title, document.getElementById('root')
+);
