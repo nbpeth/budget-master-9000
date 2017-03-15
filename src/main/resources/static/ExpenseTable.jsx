4eae3978023a0314d5a78b3825feb6675f6b518b
bud manstrong <nbpeth@gmail.com>
2017-03-14 19:11:44 -0500

    pull expense data into table

--- src/main/resources/static/ExpenseTable.jsx
+++ src/main/resources/static/ExpenseTable.jsx
@@ -1,20 +1,5 @@
 import getExpenses from './expenseService.js';
-
-
-class Expense extends React.Component {
-    render (){
-        return(
-            <tr>
-                <td>{this.props.expense.location}</td>
-                <td>${this.props.expense.cost}</td>
-                <td>{this.props.expense.expenseType}</td>
-                <td>{this.props.expense.description}</td>
-                <td>{this.props.expense.dayOfWeek}</td>
-                <td>{this.props.expense.expenseDate}</td>
-            </tr>
-        );
-    }
-};
+import Expense from './Expense.jsx';
 
 class ExpenseTable extends React.Component {
     constructor(props){
