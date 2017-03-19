dc12d05a7b69a8f94d0dd44a27acd5dd1c1fc7ef
bud manstrong <nbpeth@gmail.com>
2017-03-19 15:11:00 -0500

    set hibernate session properties

--- src/main/java/com/homebudget/Config.java
+++ src/main/java/com/homebudget/Config.java
@@ -4,16 +4,20 @@ import com.homebudget.service.ExpenseService;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories({"com.homebudget.repository"})
 @EntityScan
 @EnableTransactionManagement
+@PropertySource({"application.properties"})
 public class Config {
 
     @Bean
@@ -23,6 +27,20 @@ public class Config {
         dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/home_budget?useSSL=false");
         dataSource.setUsername("nbpeth");
         return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+//        sessionFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        hibernateProperties.put("hibernate.show_sql", "true");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "create");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+
+        return sessionFactoryBean;
     }
 
     @Bean
