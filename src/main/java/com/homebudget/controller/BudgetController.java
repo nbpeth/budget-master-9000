b7156ffe1c6423aeed6f2036eb1edbee0f5fe23f
bud manstrong <nbpeth@gmail.com>
2017-04-16 16:10:16 -0500

    test cors annotation

--- src/main/java/com/homebudget/controller/BudgetController.java
+++ src/main/java/com/homebudget/controller/BudgetController.java
@@ -9,12 +9,14 @@ import javassist.tools.web.BadHttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -29,27 +31,46 @@ public class BudgetController {
     @PostMapping("/expenses")
     public ResponseEntity<Expense> submitExpense(@RequestBody Expense expense) throws BadHttpRequest {
         expenseService.saveExpense(expense);
-        return new ResponseEntity<>(expense, HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        List<String> origins = new ArrayList<>();
+        origins.add("http://10.0.1.9");
+        headers.put("Access-Control-Allow-Origin", origins);
+        return new ResponseEntity<>(expense, headers, HttpStatus.CREATED);
     }
 
     @GetMapping("/expenses/stats")
     public ResponseEntity<Statistics> getStats(HttpServletRequest servletRequest){
-        System.out.println(servletRequest.getHeader("origin"));
-        return new ResponseEntity<>(statisticsService.getStats(), HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        List<String> origins = new ArrayList<>();
+        origins.add("http://10.0.1.9");
+        headers.put("Access-Control-Allow-Origin", origins);
+        return new ResponseEntity<>(statisticsService.getStats(), headers, HttpStatus.OK);
     }
 
     @GetMapping("/expenses")
     public ResponseEntity<Page<Expense>> getAllExpenses(Pageable pageable){
-        return new ResponseEntity<>(expenseService.getAllExpenses(pageable), HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        List<String> origins = new ArrayList<>();
+        origins.add("http://10.0.1.9");
+        headers.put("Access-Control-Allow-Origin", origins);
+        return new ResponseEntity<>(expenseService.getAllExpenses(pageable), headers, HttpStatus.OK);
     }
 
     @GetMapping("/expenses/{id}")
     public ResponseEntity<Expense> getById(@PathVariable Integer id){
-        return new ResponseEntity<>(expenseService.getById(id), HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        List<String> origins = new ArrayList<>();
+        origins.add("http://10.0.1.9");
+        headers.put("Access-Control-Allow-Origin", origins);
+        return new ResponseEntity<>(expenseService.getById(id), headers, HttpStatus.OK);
     }
 
     @DeleteMapping("/expenses/{id}")
     public void deleteById(@PathVariable Integer id){
+        HttpHeaders headers = new HttpHeaders();
+        List<String> origins = new ArrayList<>();
+        origins.add("http://10.0.1.9");
+        headers.put("Access-Control-Allow-Origin", origins);
         expenseService.deleteExpenseBy(id);
     }
 
