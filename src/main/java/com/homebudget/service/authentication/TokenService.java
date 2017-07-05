5a70bb9add7345fc8cc7f86a6758456ad38c805a
bud manstrong <nbpeth@gmail.com>
2017-07-05 16:21:12 -0500

    authenticates using google token

--- src/main/java/com/homebudget/service/authentication/TokenService.java
+++ src/main/java/com/homebudget/service/authentication/TokenService.java
@@ -1,5 +1,9 @@
 package com.homebudget.service.authentication;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.homebudget.domain.authentication.User;
 import com.homebudget.exception.UnauthorizedException;
 import io.jsonwebtoken.Claims;
@@ -11,7 +15,10 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.Key;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -52,18 +59,60 @@ public class TokenService {
         return headers;
     }
 
-    public boolean validateToken(String token) throws UnauthorizedException {
-        Claims claims;
+//    public boolean validateToken(String token) throws UnauthorizedException {
+//        Claims claims;
+//        try {
+//            claims = Jwts.parser()
+//                    .setSigningKey(DatatypeConverter.parseBase64Binary(environmentVariables.get("apiSecret")))
+//                    .parseClaimsJws(token).getBody();
+//
+//        } catch (Exception e) {
+//            throw new UnauthorizedException();
+//        }
+//
+//        return true;
+//    }
+
+    public boolean validateToken(String token) {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
+                .setAudience(Collections.singletonList("559785174037-t1mifh0tkkebo55e4v26fhiqtb70a84k.apps.googleusercontent.com"))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+// (Receive idTokenString by HTTPS POST)
+
+        GoogleIdToken idToken = null;
         try {
-            claims = Jwts.parser()
-                    .setSigningKey(DatatypeConverter.parseBase64Binary(environmentVariables.get("apiSecret")))
-                    .parseClaimsJws(token).getBody();
-
-        } catch (Exception e) {
-            throw new UnauthorizedException();
+            idToken = verifier.verify(token);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
 
-        return true;
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            System.out.println("VALIDATED");
+            System.out.println("email");
+
+            return true;
+        } else {
+            System.out.println("Invalid ID token.");
+            return false;
+        }
     }
 
 }
